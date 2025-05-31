00bea8f89497fad369aa0a52ea3b607aeb79e97e
JaouadM01 <22156569@student.hhs.nl>
2025-05-31 13:42:57 +0200

    desktop versie done

--- leapforce-case/src/components/smallcomponents/ArrowButton.jsx
+++ leapforce-case/src/components/smallcomponents/ArrowButton.jsx
@@ -1,10 +1,11 @@
 import ArrowIcon from "./ArrowIcon";
 import "../css/ArrowButton.css"
+import SvgArrow2 from "./SvgArrow2";
 
 const ArrowButton = () => {
   return (
-    <button className="arrow-button">
-      <ArrowIcon />
+    <button className="arrow-button" >
+      <SvgArrow2 />
     </button>
   );
 };
---
+++ leapforce-case/src/components/smallcomponents/SvgArrow2.jsx
@@ -0,0 +1,28 @@
+const SvgArrow2 = ({ className = "" }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <path
+        d="M6 1L12 7L6 13"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeMiterlimit="4.62023"
+      />
+      <path
+        d="M0 7L12 7"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeMiterlimit="4.62023"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SvgArrow2;
